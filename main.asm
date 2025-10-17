04103daf1ad842a4c6c5c48e3923f07c382cd21b
phanta1313 <ilyas.9murka9.ev@gmail.com>
2025-10-18 00:05:33 +0600

    scans for keyboard input

--- main.asm
+++ main.asm
@@ -1,6 +1,8 @@
 org 0x7C00
 
-%define ENDL    0x0A
+%define ENDL         0x0A
+%define VIDEO        0x10
+%define KEYBOARD     0x16
 
 %macro PRINTS 1
     mov si, %1
@@ -9,28 +11,40 @@ org 0x7C00
         or al, al          
         jz %%done
         mov ah, 0x0E 
-        int 0x10
+        int VIDEO
         jmp %%loop
     %%done:
         mov ah, 0x02
         inc dh
         mov dl, 0
-        int 0x10            ; move cusror down and left
+        int VIDEO            ; move cusror down and left
+%endmacro
+
+%macro SCANS 0
+    %%loop:
+        mov ah, 0x00
+        int 0x16
+        cmp al, 0x0D
+        jz %%done
+        mov ah, 0x0E
+        int VIDEO
+        jmp %%loop
+    %%done:
+        mov ah, 0x02
+        inc dh
+        mov dl, 0
+        int VIDEO
 %endmacro
 
 main:
     mov ax, 0x03       
-    int 0x10                ; clear the screen by refreshing video mode
- 
-    PRINTS s1
-    PRINTS s2
-    PRINTS s3
-    PRINTS s1
-    jmp $              
+    int VIDEO                ; clear the screen by refreshing video mode
+    SCANS
+    PRINTS ok
+    
+    hlt   
 
-s1 db "Hello, world!", 0
-s2 db "string2", 0
-s3 db "DSHUAKDHsao", 0
+ok: db "Ok."
 
 times 510 - ($-$$) db 0
 dw 0xAA55
